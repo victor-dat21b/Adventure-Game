fd95f39d395a33fec79a59f880ddec36ae0b7ab3
victor-dab21b <vict702k@stud.kea.dk>
2021-09-30 09:37:35 +0200

    Adventure game 1

---
+++ src/Adventure.java
@@ -0,0 +1,18 @@
+public class Adventure {
+
+
+    public static void main(String[] args) {
+        Room room1 = new Room(null, "south", "east", null);
+        Room room2 = new Room(null, null, "east", "west");
+        Room room3 = new Room(null, "south", null, "west");
+        Room room4 = new Room(null, null, null, null);
+        Room room5 = new Room(null, null, null, null);
+        Room room6 = new Room(null, null, null, null);
+        Room room7 = new Room(null, null, null, null);
+        Room room8 = new Room(null, null, null, null);
+        Room room9 = new Room(null, null, null, null);
+
+    }
+
+}
+
---
+++ src/Room.java
@@ -0,0 +1,18 @@
+public class Room {
+    private String north;
+    private String south;
+    private String east;
+    private String west;
+
+    Room(String nh, String sh, String et, String wt) {
+        this.north = nh;
+        this.south = sh;
+        this.east = et;
+        this.west = wt;
+
+
+
+
+
+    }
+}
